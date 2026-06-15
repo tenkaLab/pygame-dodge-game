733fee521a9e23a0b17f0871c1c519e8aeb018f0
dev-haku <haku.programming0803@gmail.com>
2026-06-15 22:23:00 +0900

    chore: resize screen to 640x480

--- game/config.py
+++ game/config.py
@@ -6,8 +6,8 @@ game = {
     "version" : "0.1",
     "icon_image_path" : paths.DEFAULT_ICON_IMAGE,
     
-    "window_size" : [960,540],
-    "screen_size" : [960,540],
+    "window_size" : [640*1.4, 480*1.4],
+    "screen_size" : [640, 480],
 
     "max_tps" : 30,
     "max_fps" : 30,
