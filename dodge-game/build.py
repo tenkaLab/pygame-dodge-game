1d6fb39f0e4f615995d38e9cf6fa1d18143d6c9f
dev-haku <haku.programming0803@gmail.com>
2026-06-21 12:43:56 +0900

    fix: update build.yml, update build script

--- dodge-game/build.py
+++ dodge-game/build.py
@@ -2,7 +2,7 @@ import subprocess
 import datetime
 from pathlib import Path
 
-from src.game import config
+from game import config
 
 
 _name = config.game["name"]
@@ -12,7 +12,8 @@ _icon_image_path = config.game["icon_image_path"]
 ROOT = Path(__file__).resolve().parent
 DIR_NAME = datetime.datetime.now().strftime(f"{_name}_v{_version}_%Y%m%d_%H%M%S")
 
-BUILD_ROOT = ROOT / "build" / "win" / DIR_NAME
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+BUILD_ROOT = PROJECT_ROOT / "build" / "win" / DIR_NAME
 BUILD_ROOT.mkdir(parents=True, exist_ok=True)
 
 
@@ -32,7 +33,7 @@ cmd = [
     "--distpath",    str(BUILD_ROOT / "dist"),
     "--workpath",    str(BUILD_ROOT / "build"),
     "--specpath",    str(BUILD_ROOT),
-    str((ROOT / "run.py").resolve()),
+    str((ROOT / "main.py").resolve()),
 ]
 
 subprocess.run(cmd)
